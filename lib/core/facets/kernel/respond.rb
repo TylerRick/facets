7a4830eb545bafbd2cc109c9da64a1531445cafd
Tyler Rick <tyler@tylerrick.com>
2019-03-20 09:06:59 -0700

    WIP Allow Kernel#respond to check for private/protected methods as well

--- lib/core/facets/kernel/respond.rb
+++ lib/core/facets/kernel/respond.rb
@@ -20,10 +20,17 @@ module Kernel
   #
   # CREDIT: Chris Wanstrath
 
+  # TODO: either check for private/protected methods by default, or let you pass true to have it
+  # also include private methods (same as respond_to? :sym, true works)
+  # respond(true).resource&.name || breadcrumbs.last&.name,
+
   def respond(sym=nil, *args, &blk)
-    if sym
+    case sym
+    when Symbol, String
       return nil if not respond_to?(sym)
       __send__(sym, *args, &blk)
+    #when true, false
+      #  # somehow pass this arg to Functor.new?
     else
       Functor.new(&method(:respond).to_proc)
     end
